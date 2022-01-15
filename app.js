4dfa012eb32f2a39dd08d9f67bd47469c3d7c782
Emre Gül <48657990+emregull@users.noreply.github.com>
2022-01-15 10:12:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+/* Http kutuphanesini cagiriyoruz */
 const http = require('http');
 
+/* Http ile yeni server olusturarak bunu server degiskenine atiyoruz */
 const server = http.createServer((req,res) => {
+    /* Web sayfamıza nodemon app yazdırıyoruz */
     res.write(req.url+"nodemon app");
+    /* Bitiriyoruz */
     res.end();
 })
-
+/* server localhost:3000 uzerinde aciliyor */
 server.listen(3000, () =>{
+    /* Consola asagidaki metni yazdiriyorruz */
     console.log('Server localhost:3000 uzerinde ayakta')
 })
